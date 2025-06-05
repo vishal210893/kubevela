45e1a90884ac28a16f4a649a63245710095ef8fe
vishal210893 <vishal210893@gmail.com>
2025-06-06 01:11:17 +0530

    chore: enhance logging in mutating and validating handlers for Application
    Signed-off-by: vishal210893 <vishal210893@gmail.com>

--- pkg/webhook/core.oam.dev/v1beta1/application/mutating_handler.go
+++ pkg/webhook/core.oam.dev/v1beta1/application/mutating_handler.go
@@ -50,89 +50,123 @@ var _ admission.Handler = &MutatingHandler{}
 type appMutator func(ctx context.Context, req admission.Request, oldApp *v1beta1.Application, newApp *v1beta1.Application) (bool, error)
 
 func (h *MutatingHandler) handleIdentity(_ context.Context, req admission.Request, _ *v1beta1.Application, app *v1beta1.Application) (bool, error) {
+	klog.V(1).InfoS("Entering handleIdentity", "appName", app.Name, "appNamespace", app.Namespace, "userName", req.UserInfo.Username)
 	if !utilfeature.DefaultMutableFeatureGate.Enabled(features.AuthenticateApplication) {
+		klog.V(1).InfoS("AuthenticateApplication feature gate is disabled, skipping identity handling", "appName", app.Name, "appNamespace", app.Namespace)
 		return false, nil
 	}
 
 	if slices.Contains(h.skipUsers, req.UserInfo.Username) {
+		klog.V(1).InfoS("User is in skipUsers list, skipping identity handling", "appName", app.Name, "appNamespace", app.Namespace, "userName", req.UserInfo.Username)
 		return false, nil
 	}
 
 	if metav1.HasAnnotation(app.ObjectMeta, oam.AnnotationApplicationServiceAccountName) {
+		klog.InfoS("Application has service-account annotation, which is not permitted when authentication enabled", "appName", app.Name, "appNamespace", app.Namespace)
 		return false, errors.New("service-account annotation is not permitted when authentication enabled")
 	}
 	klog.Infof("[ApplicationMutatingHandler] Setting UserInfo into Application, UserInfo: %v, Application: %s/%s", req.UserInfo, app.GetNamespace(), app.GetName())
 	auth.SetUserInfoInAnnotation(&app.ObjectMeta, req.UserInfo)
+	klog.V(1).InfoS("Successfully set UserInfo in annotation", "appName", app.Name, "appNamespace", app.Namespace)
 	return true, nil
 }
 
 func (h *MutatingHandler) handleWorkflow(_ context.Context, _ admission.Request, _ *v1beta1.Application, app *v1beta1.Application) (modified bool, err error) {
+	klog.V(1).InfoS("Entering handleWorkflow", "appName", app.Name, "appNamespace", app.Namespace)
 	if app.Spec.Workflow != nil {
+		klog.V(1).InfoS("Processing workflow steps", "appName", app.Name, "appNamespace", app.Namespace, "numSteps", len(app.Spec.Workflow.Steps))
 		for i, step := range app.Spec.Workflow.Steps {
 			if step.Name == "" {
-				app.Spec.Workflow.Steps[i].Name = fmt.Sprintf("step-%d", i)
+				newName := fmt.Sprintf("step-%d", i)
+				klog.V(1).InfoS("Mutating empty workflow step name", "appName", app.Name, "appNamespace", app.Namespace, "stepIndex", i, "newName", newName)
+				app.Spec.Workflow.Steps[i].Name = newName
 				modified = true
 			}
 			for j, sub := range step.SubSteps {
 				if sub.Name == "" {
-					app.Spec.Workflow.Steps[i].SubSteps[j].Name = fmt.Sprintf("step-%d-%d", i, j)
+					newSubName := fmt.Sprintf("step-%d-%d", i, j)
+					klog.V(1).InfoS("Mutating empty workflow sub-step name", "appName", app.Name, "appNamespace", app.Namespace, "stepIndex", i, "subStepIndex", j, "newSubName", newSubName)
+					app.Spec.Workflow.Steps[i].SubSteps[j].Name = newSubName
 					modified = true
 				}
 			}
 		}
 	}
+	klog.V(1).InfoS("Finished handleWorkflow", "appName", app.Name, "appNamespace", app.Namespace, "modified", modified)
 	return modified, nil
 }
 
 func (h *MutatingHandler) handleSharding(_ context.Context, _ admission.Request, oldApp *v1beta1.Application, newApp *v1beta1.Application) (bool, error) {
+	klog.V(1).InfoS("Entering handleSharding", "appName", newApp.Name, "appNamespace", newApp.Namespace)
 	if sharding.EnableSharding && !utilfeature.DefaultMutableFeatureGate.Enabled(features.DisableWebhookAutoSchedule) {
+		klog.V(1).InfoS("Sharding enabled and DisableWebhookAutoSchedule feature gate is not enabled", "appName", newApp.Name, "appNamespace", newApp.Namespace)
 		oid, scheduled := sharding.GetScheduledShardID(oldApp)
 		_, newScheduled := sharding.GetScheduledShardID(newApp)
 		if scheduled && !newScheduled {
 			klog.Infof("inherit old shard-id %s for app %s/%s", oid, newApp.Namespace, newApp.Name)
 			sharding.SetScheduledShardID(newApp, oid)
+			klog.V(1).InfoS("Finished handleSharding, inherited old shard-id", "appName", newApp.Name, "appNamespace", newApp.Namespace, "shardID", oid, "modified", true)
 			return true, nil
 		}
-		return sharding.DefaultScheduler.Get().Schedule(newApp), nil
+		klog.V(1).InfoS("Attempting to schedule with DefaultScheduler", "appName", newApp.Name, "appNamespace", newApp.Namespace)
+		modified := sharding.DefaultScheduler.Get().Schedule(newApp)
+		klog.V(1).InfoS("Finished handleSharding with DefaultScheduler", "appName", newApp.Name, "appNamespace", newApp.Namespace, "modified", modified)
+		return modified, nil
 	}
+	klog.V(1).InfoS("Sharding not applicable or disabled by feature gate, skipping sharding handling", "appName", newApp.Name, "appNamespace", newApp.Namespace, "EnableSharding", sharding.EnableSharding, "DisableWebhookAutoScheduleEnabled", utilfeature.DefaultMutableFeatureGate.Enabled(features.DisableWebhookAutoSchedule))
 	return false, nil
 }
 
 // Handle mutate application
 func (h *MutatingHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
+	klog.InfoS("Starting mutation for Application", "resource", req.Resource.Resource, "namespace", req.Namespace, "name", req.Name, "operation", req.Operation, "uid", req.UID)
 	oldApp, newApp := &v1beta1.Application{}, &v1beta1.Application{}
 	if err := h.Decoder.Decode(req, newApp); err != nil {
+		klog.ErrorS(err, "Failed to decode new Application from request", "uid", req.UID)
 		return admission.Errored(http.StatusBadRequest, err)
 	}
+	klog.V(1).InfoS("Successfully decoded new Application", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
+
 	if len(req.OldObject.Raw) > 0 {
 		if err := h.Decoder.DecodeRaw(req.OldObject, oldApp); err != nil {
+			klog.ErrorS(err, "Failed to decode old Application from request", "uid", req.UID)
 			return admission.Errored(http.StatusBadRequest, err)
 		}
+		klog.V(1).InfoS("Successfully decoded old Application", "appName", oldApp.Name, "appNamespace", oldApp.Namespace, "uid", req.UID)
 	}
 
 	modified := false
 	for _, handler := range []appMutator{h.handleIdentity, h.handleSharding, h.handleWorkflow} {
+		klog.V(1).InfoS("Executing app mutator", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
 		m, err := handler(ctx, req, oldApp, newApp)
 		if err != nil {
+			klog.ErrorS(err, "Application mutator failed", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
 			return admission.Errored(http.StatusBadRequest, err)
 		}
 		if m {
+			klog.V(1).InfoS("Application mutator resulted in modification", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
 			modified = true
 		}
 	}
 	if !modified {
+		klog.InfoS("No modifications made by mutators, returning Patched admission response", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
 		return admission.Patched("")
 	}
 
+	klog.InfoS("Application was modified, marshalling and creating patch response", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
 	bs, err := json.Marshal(newApp)
 	if err != nil {
+		klog.ErrorS(err, "Failed to marshal mutated Application", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID)
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
-	return admission.PatchResponseFromRaw(req.AdmissionRequest.Object.Raw, bs)
+	resp := admission.PatchResponseFromRaw(req.AdmissionRequest.Object.Raw, bs)
+	klog.InfoS("Successfully created patch response for Application", "appName", newApp.Name, "appNamespace", newApp.Namespace, "uid", req.UID, "numPatches", len(resp.Patches))
+	return resp
 }
 
 // RegisterMutatingHandler will register component mutation handler to the webhook
 func RegisterMutatingHandler(mgr manager.Manager) {
+	klog.InfoS("Registering Application mutating webhook handler", "path", "/mutating-core-oam-dev-v1beta1-applications")
 	server := mgr.GetWebhookServer()
 	handler := &MutatingHandler{
 		Decoder: admission.NewDecoder(mgr.GetScheme()),
@@ -142,4 +176,5 @@ func RegisterMutatingHandler(mgr manager.Manager) {
 		handler.skipUsers = []string{userInfo.Username}
 	}
 	server.Register("/mutating-core-oam-dev-v1beta1-applications", &webhook.Admission{Handler: handler})
+	klog.InfoS("Application mutating webhook handler registered successfully")
 }
--- pkg/webhook/core.oam.dev/v1beta1/application/validating_handler.go
+++ pkg/webhook/core.oam.dev/v1beta1/application/validating_handler.go
@@ -24,6 +24,7 @@ import (
 
 	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -62,43 +63,70 @@ func mergeErrors(errs field.ErrorList) error {
 
 // Handle validate Application Spec here
 func (h *ValidatingHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
+	klog.InfoS("Starting validation for Application",
+		"resource", req.Resource.Resource, "namespace", req.Namespace, "name", req.Name, "operation", req.Operation, "uid", req.UID)
 	app := &v1beta1.Application{}
 	if err := h.Decoder.Decode(req, app); err != nil {
+		klog.ErrorS(err, "Failed to decode Application from request", "uid", req.UID)
 		return admission.Errored(http.StatusBadRequest, err)
 	}
+	klog.V(1).InfoS("Successfully decoded Application for validation", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
+
 	if req.Namespace != "" {
+		klog.V(1).InfoS("Setting Application namespace from request namespace", "appName", app.Name, "appNamespace", app.Namespace, "requestNamespace", req.Namespace, "uid", req.UID)
 		app.Namespace = req.Namespace
 	}
 
 	ctx = util.SetNamespaceInCtx(ctx, app.Namespace)
+	klog.V(1).InfoS("Set namespace in context for Application validation", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
+
 	switch req.Operation {
 	case admissionv1.Create:
+		klog.InfoS("Processing CREATE operation for Application", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
 		if allErrs := h.ValidateCreate(ctx, app); len(allErrs) > 0 {
+			mergedErr := mergeErrors(allErrs)
+			klog.InfoS("Application CREATE validation failed", "appName", app.Name, "appNamespace", app.Namespace, "error", mergedErr.Error(), "uid", req.UID)
 			// http.StatusUnprocessableEntity will NOT report any error descriptions
 			// to the client, use generic http.StatusBadRequest instead.
-			return admission.Errored(http.StatusBadRequest, mergeErrors(allErrs))
+			return admission.Errored(http.StatusBadRequest, mergedErr)
 		}
+		klog.InfoS("Application CREATE validation successful", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
 	case admissionv1.Update:
+		klog.InfoS("Processing UPDATE operation for Application", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
 		oldApp := &v1beta1.Application{}
 		if err := h.Decoder.DecodeRaw(req.AdmissionRequest.OldObject, oldApp); err != nil {
-			return admission.Errored(http.StatusBadRequest, simplifyError(err))
+			simplifiedErr := simplifyError(err)
+			klog.ErrorS(simplifiedErr, "Failed to decode old Application from request for UPDATE", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
+			return admission.Errored(http.StatusBadRequest, simplifiedErr)
 		}
+		klog.V(1).InfoS("Successfully decoded old Application for UPDATE validation", "appName", oldApp.Name, "appNamespace", oldApp.Namespace, "uid", req.UID)
 		if app.ObjectMeta.DeletionTimestamp.IsZero() {
+			klog.V(1).InfoS("Application is not being deleted, proceeding with UPDATE validation", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
 			if allErrs := h.ValidateUpdate(ctx, app, oldApp); len(allErrs) > 0 {
-				return admission.Errored(http.StatusBadRequest, mergeErrors(allErrs))
+				mergedErr := mergeErrors(allErrs)
+				klog.InfoS("Application UPDATE validation failed", "appName", app.Name, "appNamespace", app.Namespace, "error", mergedErr.Error(), "uid", req.UID)
+				return admission.Errored(http.StatusBadRequest, mergedErr)
 			}
+			klog.InfoS("Application UPDATE validation successful", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
+		} else {
+			klog.InfoS("Application is being deleted, skipping UPDATE validation", "appName", app.Name, "appNamespace", app.Namespace, "uid", req.UID)
 		}
 	default:
+		klog.InfoS("Operation is not CREATE or UPDATE for Application. No specific validation rules applied for this operation.",
+			"appName", app.Name, "appNamespace", app.Namespace, "operation", req.Operation, "uid", req.UID)
 		// Do nothing for DELETE and CONNECT
 	}
+	klog.InfoS("Application validation request allowed", "appName", app.Name, "appNamespace", app.Namespace, "operation", req.Operation, "uid", req.UID)
 	return admission.ValidationResponse(true, "")
 }
 
 // RegisterValidatingHandler will register application validate handler to the webhook
 func RegisterValidatingHandler(mgr manager.Manager, _ controller.Args) {
+	klog.InfoS("Registering Application validating webhook handler", "path", "/validating-core-oam-dev-v1beta1-applications")
 	server := mgr.GetWebhookServer()
 	server.Register("/validating-core-oam-dev-v1beta1-applications", &webhook.Admission{Handler: &ValidatingHandler{
 		Client:  mgr.GetClient(),
 		Decoder: admission.NewDecoder(mgr.GetScheme()),
 	}})
+	klog.InfoS("Application validating webhook handler registered successfully", "path", "/validating-core-oam-dev-v1beta1-applications")
 }
